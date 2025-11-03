496c62b453d62235bd4977c84d7af4d60b047853
Pankaj Ugale <pankajugale2020@gmail.com>
2025-11-03 10:20:25 +0530

    Adding new Class.

--- src/assignments/student_submissions/Pankaj_Ugale/Demo.java
+++ src/assignments/student_submissions/Pankaj_Ugale/Demo.java
@@ -2,7 +2,7 @@ package assignments.student_submissions.Pankaj_Ugale;
 
 public class Demo {
     static void main() {
-        System.out.println("Hello World");
+        System.out.println("Hello World..");
     }
 
 }
