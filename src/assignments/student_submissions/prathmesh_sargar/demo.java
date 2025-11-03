b3b7d1ddcc983e84e0886e2a4fc479f1b0ce8387
Prathmesh <prathmeshsargar10@gmail.com>
2025-11-03 10:07:20 +0530

    A demo code added to demonstration package strategy

--- src/assignments/student_submissions/prathmesh_sargar/demo.java
+++ src/assignments/student_submissions/prathmesh_sargar/demo.java
@@ -2,6 +2,6 @@ package assignments.student_submissions.prathmesh_sargar;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Java");
     }
 }
