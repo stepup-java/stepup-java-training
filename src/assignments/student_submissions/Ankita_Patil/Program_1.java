afe0a1e457376e5b36e2bc4372e2b62fe5a54f80
patil-Ankita <pankita0507@gmail.com>
2025-11-03 10:31:56 +0530

    Demo Code of the First Program

---
+++ src/assignments/student_submissions/Ankita_Patil/Program_1.java
@@ -0,0 +1,7 @@
+package assignments.student_submissions.Ankita_Patil;
+
+public class Program_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Full Stack Developers");
+    }
+}
