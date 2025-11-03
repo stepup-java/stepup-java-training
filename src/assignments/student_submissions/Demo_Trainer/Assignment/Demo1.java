3a5742f3d7d594e954122388d9f2cd35e4887ac6
Rituja Nimbalkar <ritujanimbalkar22@gmail.com>
2025-11-03 11:58:59 +0530

    add Demo4 file successfully

--- src/assignments/student_submissions/Demo_Trainer/Assignment/Demo1.java
+++ src/assignments/student_submissions/Demo_Trainer/Assignment/Demo1.java
@@ -2,6 +2,7 @@ package assignments.student_submissions.Demo_Trainer.Assignment;
 
 public class Demo1 {
     public static void main (String[] args){
-        System.out.println("Hello step up and learn    ");
+        System.out.println("Hello step up and learn");
+        System.out.println("Hello step up and learn");
     }
 }
--- src/assignments/student_submissions/Demo_Trainer/Assignment/demo2.java
+++ src/assignments/student_submissions/Demo_Trainer/Assignment/demo2.java
@@ -4,5 +4,6 @@ public class demo2 {
     public static void main (String[] args){
         System.out.println("Hello step up and learn");
         System.out.println("Hello step up and learn");
+        System.out.println("Hello step up and learn");
     }
 }
