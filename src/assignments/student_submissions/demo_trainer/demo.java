4f78118c19d32de986eef69bcf4ef1d7d23cd5c4
Sakshiideshmane <sakshideshmane04@gmail.com>
2025-11-03 10:26:17 +0530

    A demo code addedd

--- src/assignments/student_submissions/demo_trainer/demo.java
+++ src/assignments/student_submissions/demo_trainer/demo.java
@@ -2,7 +2,7 @@ package assignments.student_submissions.demo_trainer;
 
 public class demo {
     public static void main(String [] args){
-        System.out.println("Hello demooo");
+        System.out.println("Hello demoooo");
     }
 
 }
