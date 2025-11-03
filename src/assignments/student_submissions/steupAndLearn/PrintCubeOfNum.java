c8c91e9dedc98a8e609677d1fd8bbb8247fb2a06
Neha Ingawale <nehaingawale48@gmail.com>
2025-11-03 22:23:37 +0530

    A demo code addewd for demostration of beanching Edit Demo file

---
+++ src/assignments/student_submissions/steupAndLearn/PrintCubeOfNum.java
@@ -0,0 +1,14 @@
+                                  /* Print cubes of numbers from 1 to 10*/
+
+package assignments.student_submissions.steupAndLearn;
+
+public class PrintCubeOfNum {
+
+    public static void main(String[] args) {
+
+        for (int i=1; i<=10; i++)
+        {
+            System.out.println("Cube of " + i + " = " + (i*i*i));
+        }
+    }
+}
---
+++ src/assignments/student_submissions/steupAndLearn/PrintNumDividedBy.java
@@ -0,0 +1,17 @@
+                             /*Print numbers divisible by 3 from 1 to 50*/
+
+package assignments.student_submissions.steupAndLearn;
+
+public class PrintNumDividedBy {
+
+    public static void main(String[] args) {
+
+        for (int i=1; i<=50; i++)
+        {
+            if (i % 3 == 0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
