e99386e92015064b972eecdc87c1eb4a740aa1ee
Prathmesh <prathmeshsargar10@gmail.com>
2025-11-03 10:34:54 +0530

    A demo new code added to demonstration package strategy - added code package

--- src/assignments/student_submissions/prathmesh_s/demo1.java
+++ src/assignments/student_submissions/prathmesh_s/demo1.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class demo1 {
     public static void main(String[] args) {
         System.out.println("Hello sir");
+        System.out.println("Hello GitHub");
     }
 }
