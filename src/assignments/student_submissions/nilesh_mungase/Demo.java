607ccdb669fce9ead4ba4cc31129eb2dd69dac63
Nilesh Mungase <nileshmungase101@gmail.com>
2025-11-03 10:25:08 +0530

    first commit

--- src/assignments/student_submissions/nilesh_mungase/Demo.java
+++ src/assignments/student_submissions/nilesh_mungase/Demo.java
@@ -2,6 +2,6 @@ package assignments.student_submissions.nilesh_mungase;
 
 public class Demo {
     static void main() {
-        System.out.println("Hello Nilesh");
+        System.out.println("Hello Nilesh ");
     }
 }
