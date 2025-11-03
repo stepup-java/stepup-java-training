39cc7fbd07918ea52f3d56f63125961bd1b7c5ba
Rituja Nimbalkar <ritujanimbalkar22@gmail.com>
2025-11-03 12:49:24 +0530

    added file

---
+++ src/classroom_trainings/Assignment/Assignment1.java
@@ -0,0 +1,25 @@
+package classroom_trainings.Assignment;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int a=90;
+        int b=59;
+
+        //1st way
+        a=a^b;
+        System.out.println(b=a^b);
+        System.out.println(a=a^b);
+
+        //2nd way
+        a=a+b;
+        System.out.println(b=a-b);
+        System.out.println(a=a-b);
+
+        //3rd way
+        int temp;
+        temp=a;
+        System.out.println(a=b);
+        System.out.println(b=temp);
+    }
+}
