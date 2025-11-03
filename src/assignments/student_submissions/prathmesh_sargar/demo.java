b9a82ebe7d7a074e372070073f3f9ec0182bc1f9
Prathmesh <prathmeshsargar10@gmail.com>
2025-11-03 10:13:38 +0530

    A demo code added to demonstration package strategy 3

--- src/assignments/student_submissions/prathmesh_sargar/demo.java
+++ src/assignments/student_submissions/prathmesh_sargar/demo.java
@@ -3,5 +3,6 @@ package assignments.student_submissions.prathmesh_sargar;
 public class demo {
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        System.out.println("Trying to push on Github");
     }
 }
