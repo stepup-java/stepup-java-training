9de68a71db9507230a98e8da34e8def044a77791
Prathmesh <prathmeshsargar10@gmail.com>
2025-11-03 10:26:15 +0530

    A demo code added to demonstration package strategy - added second code package

---
+++ src/assignments/student_submissions/prathmesh_sargar/Github_practice.java
@@ -0,0 +1,8 @@
+package assignments.student_submissions.prathmesh_sargar;
+
+public class Github_practice {
+    public static void main(String[] args) {
+
+        System.out.println("This is second demo");
+    }
+}
