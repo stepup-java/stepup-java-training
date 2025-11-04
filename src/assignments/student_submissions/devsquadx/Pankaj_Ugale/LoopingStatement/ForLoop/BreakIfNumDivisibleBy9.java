90e8be45e4915e8328654000ffcd6199bb077358
Pankaj Ugale <pankajugale2020@gmail.com>
2025-11-05 00:01:32 +0530

    Adding new folders.

---
+++ src/assignments/student_submissions/devsquadx/Pankaj_Ugale/LoopingStatement/ForLoop/BreakIfNumDivisibleBy9.java
@@ -0,0 +1,15 @@
+package assignments.student_submissions.devsquadx.Pankaj_Ugale.LoopingStatement.ForLoop;
+
+public class BreakIfNumDivisibleBy9 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=20; i++)
+		{
+			if(i%9==0) {
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+}
