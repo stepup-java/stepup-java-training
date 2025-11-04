931c6842d429dbb3ffb4fc73ab5cfa5fd00ed205
Pankaj Ugale <pankajugale2020@gmail.com>
2025-11-04 22:23:17 +0530

    Adding new folders

---
+++ src/assignments/student_submissions/Pankaj_Ugale/LoopingStatement/WhileLoop/PrintNumber1To10.java
@@ -0,0 +1,10 @@
+package assignments.student_submissions.Pankaj_Ugale.LoopingStatement.WhileLoop;
+
+public class PrintNumber1To10 {
+public static void main(String[] args) {
+	for(int i =1; i<=10;i++)
+	{
+		System.out.println(i);
+	}
+}
+}
