74ce0ed1a5d78efad08478828f159ad2b95377d7
harshadthube <harshadthube20@gmail.com>
2025-11-04 12:29:56 +0530

    add dots

--- src/assignments/student_submissions/harshadthube/Demo2.java
+++ src/assignments/student_submissions/harshadthube/Demo2.java
@@ -4,6 +4,7 @@ public class Demo2
 {
     static void main(String[] args)
     {
-        System.out.println("world welcoming");
+        System.out.println("world welcoming.....");
+        System.out.println("world welcoming.....");
     }
 }
