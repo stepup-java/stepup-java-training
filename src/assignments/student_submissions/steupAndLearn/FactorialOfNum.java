bc9e4a57a8632cf8daa3c64f5bec27319489b1df
Neha Ingawale <nehaingawale48@gmail.com>
2025-11-04 11:31:03 +0530

    A demo code addewd for demostration of beanching Edit Demo files

---
+++ src/assignments/student_submissions/steupAndLearn/FactorialOfNum.java
@@ -0,0 +1,14 @@
+package assignments.student_submissions.steupAndLearn;
+
+public class FactorialOfNum {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        while (num == 0)
+        {
+
+        }
+    }
+}
