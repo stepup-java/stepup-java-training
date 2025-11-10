ce68b353f65600b764f02bb2c1c2e82d638c6e50
Prathmesh <prathmeshsargar10@gmail.com>
2025-11-10 22:48:27 +0530

    make package for assignment and make assignment

---
+++ src/assignments/student_submissions/fullstackninjas/prathmesh_sargar/first_assignments/TableOfFive.java
@@ -0,0 +1,11 @@
+package assignments.student_submissions.fullstackninjas.prathmesh_sargar.first_assignments;
+
+// Q5: Print table of 5
+public class TableOfFive {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++) {
+            System.out.print(5*i + " ");
+        }
+    }
+}
+
