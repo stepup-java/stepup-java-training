5fcab3b8ab7f47099043525506274c69187bcad8
vijay chopade <vijayChopade9211@gmail.com>
2025-11-10 17:12:14 +0530

    HelloSelenium Commit

---
+++ src/assignments/student_submissions/codecrafters/vijay_chopade/dailytask/day_01/HelloSelenium.java
@@ -0,0 +1,11 @@
+package assignments.student_submissions.codecrafters.vijay_chopade.dailytask.day_01;
+
+
+public class HelloSelenium {
+    public static void main(String[] args) {
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://selenium.dev");
+//        driver.quit();
+        System.out.println("Hello Selenium");
+    }
+}
