ab3389819928744bd329f7220ccdeab39349214a
Prajwalk-20 <prajwalkalaskar93@gmail.com>
2025-11-11 16:57:28 +0530

    I have done some changes in Q1

--- src/assignments/student_submissions/buglife2point0/prajwal_kalaskar/Prajwal_assignment/Q1.java
+++ src/assignments/student_submissions/buglife2point0/prajwal_kalaskar/Prajwal_assignment/Q1.java
@@ -2,7 +2,7 @@ package assignments.student_submissions.buglife2point0.prajwal_kalaskar.Prajwal_
 
 public class Q1 {
     public static void main(String[] args) {
-       // Print numbers from 1 to 10
+
 
          for(int i=1; i<=10; i++) {
 			System.out.println(i);
